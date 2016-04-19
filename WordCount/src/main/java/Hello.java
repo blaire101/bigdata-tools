0d569335cb55d9071b953f387a1eda5e40e9da45
bean-vip <bean_vip@126.com>
2016-04-19 17:00:10 +0800

    add spark project

---
+++ WordCount/src/main/java/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Author : clb@x.com
+ * Date : 2016-03-25
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello Wolrd for java");
+    }
+}
